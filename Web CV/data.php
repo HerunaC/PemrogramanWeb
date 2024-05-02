6ddea96e1e238802b5cb3401081e53f5a2663bef
Heruna <165206144+HerunaC@users.noreply.github.com>
2024-05-02 22:20:54 +0900

    Add files via upload

---
+++ Web CV/data.php
@@ -0,0 +1,65 @@
+<?php
+
+$biodata = array(
+    "Nama" => "Kadek Dito Mitabhaheruna",
+    "Umur" => "21",
+    "Alamat" => "Pemogan, Bali",
+    "Tempat/Tanggal Lahir" => "Singaraja, 13 Juni 2002",
+    "Nomor HP" => "+6281246427185"
+);
+
+$work_experience = array(
+    array(
+        "Position" => "Trainee",
+        "Company" => "Keraton Jimbaran Beach Resort",
+        "Timeframe" => "Jan 2018"
+    ),
+    array(
+        "Position" => "Trainee",
+        "Company" => "The Pavillion Bali",
+        "Timeframe" => "Jan 2019"
+    ),
+    array(
+        "Position" => "Freelancing",
+        "Company" => "Freelance Websites",
+        "Timeframe" => "2020 - Present"
+    )
+);
+
+$education = array(
+    array(
+        "School" => "SDN 12 Sanur",
+        "Level" => "Elementary School",
+        "Dates" => "2008 - 2014"
+    ),
+    array(
+        "School" => "SMP Wisata Sanur",
+        "Level" => "Junior High School",
+        "Dates" => "2014 - 2017"
+    ),
+    array(
+        "School" => "SMK Teknologi Wirabhakti",
+        "Level" => "Senior High School",
+        "Major" => "Perhotelan",
+        "Dates" => "2017 - 2020"
+    ),
+    array(
+        "University" => "University of Example",
+        "Major" => "Information and Data Management IT",
+        "Dates" => "2023 - Present"
+    )
+);
+
+$skills = array(
+    "Programming Languages: LUA, PHP, JavaScript, Python",
+    "Web Technologies: HTML5, CSS3, jQuery",
+    "Database: MySQL",
+    "Other: Agile Development, Problem Solving"
+);
+
+$hobby = array(
+    "Playing Chess",
+    "Gaming"
+);
+
+?>
